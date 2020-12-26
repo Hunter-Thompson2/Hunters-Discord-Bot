370e51d1f3a6dd021bab63cdbc2a9c984e8935af
Hunter-Thompson2 <hunterthompson2@my.unt.edu>
2020-12-25 18:06:40 -0600

    tried fixing /leave
    /leave command doesnt work. dont include .env

--- HuntersBot.js
+++ HuntersBot.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+var connection;
 client.login(process.env.Token);
 
 client.on('ready', () => {
@@ -16,14 +16,20 @@ async function commandHandler(msg) {
 
     if (msg.content === '/join') {
         if (msg.member.voice.channel) {
-            const connection = await msg.member.voice.channel.join();
+            connection = await msg.member.voice.channel.join();
         } else {
             msg.reply('You need to join a voice channel first!');
         }
+        return;
     }
     else if (msg.content === '/leave') {
         if (msg.member.voice.channel) {
-            msg.member.voice.channel.disonnect();
+            //client.leaveVoiceChannel(msg.member.voice.channel);
+            msg.guild.voiceConnection.disconnect();
+            console.log('leaving channel!')
         }
+        else
+            msg.reply('you have to be in the voice channel first!')
+        return;
     }
 }
